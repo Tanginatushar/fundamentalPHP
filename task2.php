98a53d88c447c628e82bc36aa38d50e9dac077a1
shajed <ctgboy2010@gmail.com>
2015-01-08 14:43:42 +0600

    tasks of HTML

--- task2.php
+++ task2.php
@@ -3,6 +3,8 @@
 //task 1
 echo "Agent: who do you think you are, anyhow?</br>";
 echo "Neo: I am Neo, but my people call me the One</br>";
+
+
 // Task 2
 $actor_agent="Agent" ;
 echo "$actor_agent.:who do you think you are, anyhow?</br>";
---
+++ task3.php
@@ -0,0 +1,5 @@
+<?php
+$actor='Neo';
+$anomaly='Anomaly';
+$serial='1';
+echo "$actor : I am $actor, the $anomaly.You can call me by my serial number ,$serial.";
