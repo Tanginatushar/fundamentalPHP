32ac21c36f2c7927c45e59f8b3029b98426a24db
shajed <ctgboy2010@gmail.com>
2015-01-08 09:30:21 +0600

    task 2 of PHP fundamental

--- task2.php
+++ task2.php
@@ -1,12 +1,12 @@
 
 <?php
+//task 1
 echo "Agent: who do you think you are, anyhow?</br>";
-
 echo "Neo: I am Neo, but my people call me the One</br>";
-
-$agent="Agent" ;
-echo "$agent".":who do you think you are, anyhow?"."</br>";
-$neo="Neo";
-echo "$neo".": I am $neo,but my people call me the One";
+// Task 2
+$actor_agent="Agent" ;
+echo "$actor_agent.:who do you think you are, anyhow?</br>";
+$actor_neo="Neo";
+echo "$actor_neo.: I am $actor_neo,but my people call me the One";
 
 
